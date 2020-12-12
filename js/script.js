3c1760f54b5c560db0548770378eba0ff887c2ca
clintonhays <clintonhays84@gmail.com>
2020-12-12 16:39:48 +0900

    display and hide other job role input element

--- js/script.js
+++ js/script.js
@@ -3,6 +3,26 @@
  */
 
 const nameInput = document.getElementById('name');
+const jobRoleSelect = document.querySelector('#title');
+const jobRoleOptions = document.querySelectorAll('#title option');
+const otherRole = document.getElementById('other-job-role');
 
-// load page with focus in name input element
+/**
+ * on page load settings
+ */
+
+// focus on name input element
 nameInput.focus();
+
+// hide other job role input element
+otherRole.classList.add('hidden');
+
+/**
+ * Event Listeners
+ */
+
+jobRoleSelect.addEventListener('change', (e) => {
+  if (e.target.value === 'other') {
+    otherRole.classList.remove('hidden');
+  }
+});
